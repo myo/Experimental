7a0b8e2f7d315a07b6832d19246bbae3e81c2e0f
Alex <thinkinginbinary@bk.ru>
2015-12-02 07:23:32 +0200

    fix turret jihads

--- MySharpSupport/AI/HeatMap.cs
+++ MySharpSupport/AI/HeatMap.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
+using LeagueSharp.SDK.Core.Utils;
 using SharpDX;
 using SharpDX.Direct3D9;
 
@@ -21,6 +22,17 @@ namespace MySharpSupport.AI
                 //create a polygon to determine where the bot should stick at all the time
                 var myLimitPolygon = new Geometry.Circle(Core.Carry.ServerPosition, Core.MaxDistanceFromCarry)
                         .ToPolygon();
+
+                //stay out of turret range
+                var turret =
+                    ObjectManager.Get<Obj_AI_Turret>()
+                        .FirstOrDefault(t => t.IsEnemy && t.IsValidTarget() && t.Distance(ObjectManager.Player) <= 800);
+
+                if (turret != null &&
+                    !MinionManager.GetMinions(1000, MinionTypes.All, MinionTeam.Ally).Any(m => m.Distance(turret) < 800))
+                {
+                    return myLimitPolygon.Points.OrderByDescending(point => point.Distance(turret.ServerPosition)).FirstOrDefault().To3D();
+                }
                 
                 //we take the enemy that is closest to us
                 var enemyClosestToMe =
