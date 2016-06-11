b54b7a22becad0374144d634ddefbcc31a30571b
Lucas Vuigner <lucas.vuigner@gmail.com>
2016-06-11 18:22:07 +0200

    reborn
    reborn

--- AutoSharp/Properties/AssemblyInfo.cs
+++ AutoSharp/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoSharp")]
+[assembly: AssemblyTitle("AutoSharp Reborn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeagueSharp")]
-[assembly: AssemblyProduct("AutoSharp")]
-[assembly: AssemblyCopyright("Copyright © LeagueSharp 2015")]
+[assembly: AssemblyProduct("AutoSharp Reborn")]
+[assembly: AssemblyCopyright("Copyright © LeagueSharp 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.5.3009")]
-[assembly: AssemblyFileVersion("0.1.5.3009")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
